9cc70c209a29d04abebdd92d1b3db01686637a82
benleiby <105562274+benleiby@users.noreply.github.com>
2025-02-05 21:08:38 -0500

    Created Spring Project

---
+++ syncura360_spring/src/main/java/com/example/syncura360_spring/Syncura360SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.syncura360_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Syncura360SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Syncura360SpringApplication.class, args);
+	}
+
+}
---
+++ syncura360_spring/src/test/java/com/example/syncura360_spring/Syncura360SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syncura360_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Syncura360SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
