affcdc784950c28da1421bc08d2205f086dd7fa2
benleiby <105562274+benleiby@users.noreply.github.com>
2025-03-19 12:44:40 -0400

    SYN-64 Cleanup batch update method in StaffController.

--- syncura360_spring/src/main/java/com/syncura360/unorganized/StaffUpdateRequest.java
+++ syncura360_spring/src/main/java/com/syncura360/unorganized/StaffUpdateRequest.java
@@ -10,7 +10,7 @@ public class StaffUpdateRequest {
 
     @Data
     public static class StaffUpdateDto {
-        private Integer id;
+        private String username;
         private Map<String, Object> fields;
     }
 
