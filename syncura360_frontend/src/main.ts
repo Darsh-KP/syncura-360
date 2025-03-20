910e9e27ca63692db63533d0caac78e11c90f171
Ayush Gupta <97930251+ayushkgu@users.noreply.github.com>
2025-03-19 22:10:19 -0400

    SYN-85 prevent user from navigating to dashboard if not logged in

--- syncura360_frontend/src/main.ts
+++ syncura360_frontend/src/main.ts
@@ -8,15 +8,13 @@ import { RegisterComponent } from './app/components/register/register.component'
 import { DashboardComponent } from './app/components/dashboard/dashboard.component';
 import { MatNativeDateModule } from '@angular/material/core';
 import { importProvidersFrom } from '@angular/core';
+import { AuthGuard } from './app/guards/auth.guard';
+import { routes } from './app/app.routes';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter([
-      { path: '', component: LoginComponent }, // Default Login Page
-      { path: 'register', component: RegisterComponent }, 
-      { path: 'dashboard', component: DashboardComponent }, 
-    ]),
+    provideRouter(routes),  // Use imported routes
     provideHttpClient(withFetch()),
     provideAnimations(),
     importProvidersFrom(MatNativeDateModule),
