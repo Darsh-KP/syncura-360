eddee2d0a1553132fa09319a752d32ba5842bd6c
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2025-03-20 22:50:20 -0400

    SYN-86: Created empty drug controller.

---
+++ syncura360_spring/src/main/java/com/syncura360/controller/DrugController.java
@@ -0,0 +1,16 @@
+package com.syncura360.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Handles inserting, editing, and deleting drugs from inventory.
+ * @author Darsh-KP
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/drug")
+public class DrugController {
+    
+}
