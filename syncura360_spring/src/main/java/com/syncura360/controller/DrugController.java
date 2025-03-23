f33e8ffa4ea6236fc323045600bab5b5f2ca84be
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2025-03-23 12:30:27 -0400

    SYN-90: Added javadoc for Drug Controller.

--- syncura360_spring/src/main/java/com/syncura360/controller/DrugController.java
+++ syncura360_spring/src/main/java/com/syncura360/controller/DrugController.java
@@ -14,7 +14,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Handles inserting, editing, deleting, and viewing drugs from inventory.
+ * Controller for managing drugs in the inventory.
+ * Handles operations for inserting, editing, deleting, and viewing drug information.
  * @author Darsh-KP
  */
 @RestController
@@ -24,12 +25,27 @@ public class DrugController {
     JwtUtil jwtUtil;
     DrugService drugService;
 
-    // Constructor injection
+    /**
+     * Constructor for initializing DrugController with required dependencies.
+     * Uses constructor injection for necessary components.
+     *
+     * @param jwtUtil Used to access jwt claims.
+     * @param drugService The service layer used for performing operations related to drugs.
+     */
     public DrugController(JwtUtil jwtUtil, DrugService drugService) {
         this.jwtUtil = jwtUtil;
         this.drugService = drugService;
     }
 
+    /**
+     * Processes the HTTP request to add a new drug in the system.
+     * Validates drug data, and stores it in the database.
+     *
+     * @param authorization The authorization token used to identify user making the request.
+     * @param drugFormDTO DTO containing the details of the drug to be added.
+     * @param bindingResult Holds the results of the validation of the {@link DrugFormDTO}. This allows for checking if the data is valid before proceeding.
+     * @return A {@link ResponseEntity} containing a {@link GenericMessageResponseDTO} that indicated whether the drug was successfully added or if an error occurred.
+     */
     @PostMapping
     public ResponseEntity<GenericMessageResponseDTO> addDrug(
             @RequestHeader(name="Authorization") String authorization, @Valid @RequestBody DrugFormDTO drugFormDTO, BindingResult bindingResult) {
@@ -54,6 +70,15 @@ public class DrugController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new GenericMessageResponseDTO("Successfully added the drug."));
     }
 
+    /**
+     * Processes the HTTP request to update an existing drug in the system.
+     * Validates drug data, and updates it in the database.
+     *
+     * @param authorization The authorization token used to identify user making the request.
+     * @param drugFormDTO DTO containing the details of the drug to be updated.
+     * @param bindingResult Holds the results of the validation of the {@link DrugFormDTO}. This allows for checking if the data is valid before proceeding.
+     * @return A {@link ResponseEntity} containing a {@link GenericMessageResponseDTO} that indicated whether the drug was successfully updated or if an error occurred.
+     */
     @PutMapping
     public ResponseEntity<GenericMessageResponseDTO> modifyDrug(
             @RequestHeader(name="Authorization") String authorization, @Valid @RequestBody DrugFormDTO drugFormDTO, BindingResult bindingResult) {
@@ -78,6 +103,15 @@ public class DrugController {
         return ResponseEntity.status(HttpStatus.OK).body(new GenericMessageResponseDTO("Successfully updated the drug."));
     }
 
+    /**
+     * Processes the HTTP request to delete an existing drug in the system.
+     * Validates drug data, and removes it from the database.
+     *
+     * @param authorization The authorization token used to identify user making the request.
+     * @param drugDeletionDTO DTO containing the details of the drug to be removed.
+     * @param bindingResult Holds the results of the validation of the {@link DrugDeletionDTO}. This allows for checking if the data is valid before proceeding.
+     * @return A {@link ResponseEntity} containing a {@link GenericMessageResponseDTO} that indicated whether the drug was successfully deleted or if an error occurred.
+     */
     @DeleteMapping
     public ResponseEntity<GenericMessageResponseDTO> removeDrug(
             @RequestHeader(name="Authorization") String authorization, @Valid @RequestBody DrugDeletionDTO drugDeletionDTO, BindingResult bindingResult) {
@@ -102,6 +136,12 @@ public class DrugController {
         return ResponseEntity.status(HttpStatus.OK).body(new GenericMessageResponseDTO("Successfully removed the drug."));
     }
 
+    /**
+     * Fetches a list of drugs from the inventory.
+     *
+     * @param authorization The authorization token used to identify user making the request.
+     * @return A {@link ResponseEntity} containing a {@link DrugFetchListDTO} with a list of drugs. Only sends necessary information contained in {@link DrugFormDTO}.
+     */
     @GetMapping
     public ResponseEntity<DrugFetchListDTO> getDrugs(@RequestHeader(name="Authorization") String authorization) {
         // Get the hospital id the logged in staff works at
