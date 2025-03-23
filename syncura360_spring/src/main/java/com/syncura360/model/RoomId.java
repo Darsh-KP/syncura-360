4517020ea2f7b1efaaa39878e9d9fb0168357ff4
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2025-03-23 15:42:10 -0400

    SYN-87: Finalized JPA entities for room management.

--- syncura360_spring/src/main/java/com/syncura360/model/RoomId.java
+++ syncura360_spring/src/main/java/com/syncura360/model/RoomId.java
@@ -3,21 +3,31 @@ package com.syncura360.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 
+import java.io.Serial;
 import java.util.Objects;
 
+@NoArgsConstructor(force = true)
 @Getter
 @Setter
 @Embeddable
 public class RoomId implements java.io.Serializable {
+    @Serial
     private static final long serialVersionUID = -2835522800506666135L;
-    @Column(name = "hospital_id", nullable = false)
-    private Integer hospitalId;
 
-    @Column(name = "room_no", nullable = false)
-    private Integer roomNo;
+    @Column(name = "hospital_id", nullable = false)
+    private final Integer hospitalId;
+
+    @Column(name = "room_name", nullable = false, length = 50)
+    private final String roomName;
+
+    public RoomId(Integer hospitalId, String roomName) {
+        this.hospitalId = hospitalId;
+        this.roomName = roomName;
+    }
 
     @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
     @Override
@@ -25,12 +35,12 @@ public class RoomId implements java.io.Serializable {
         if (this == o) return true;
         if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         RoomId entity = (RoomId) o;
-        return Objects.equals(this.roomNo, entity.roomNo) &&
+        return Objects.equals(this.roomName, entity.roomName) &&
                 Objects.equals(this.hospitalId, entity.hospitalId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(roomNo, hospitalId);
+        return Objects.hash(roomName, hospitalId);
     }
 }
