717f009b9730d5c4c3fc9dcd4579b829f4272cd5
Mubbashir Mahmood <mm3229@scarletmail.rutgers.edu>
2025-03-27 13:32:52 -0400

    SYN-81: FinishedDesign

--- syncura360_frontend/src/app/app.routes.ts
+++ syncura360_frontend/src/app/app.routes.ts
@@ -4,10 +4,16 @@ import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 import { SchedulingComponent } from './components/scheduling/scheduling.component';
+import {InventoryComponent} from './components/inventory/inventory.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent }, 
-  { path: 'register', component: RegisterComponent },  
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'scheduling', component: SchedulingComponent, canActivate: [AuthGuard] },
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'inventory', component: InventoryComponent },
+
 ];
