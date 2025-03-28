89230ce488cbee21372f590bf94ed0c3d600e6a0
Ayush Gupta <97930251+ayushkgu@users.noreply.github.com>
2025-03-27 20:09:49 -0400

    SYN-93 updated date of birth format

--- syncura360_frontend/src/app/components/nurse-maint/register-patient/register-patient.component.ts
+++ syncura360_frontend/src/app/components/nurse-maint/register-patient/register-patient.component.ts
@@ -101,7 +101,11 @@ export class RegisterPatientComponent {
 
   formatDate(date: any): string {
     const d = new Date(date);
-    return d.toISOString().split('T')[0];
+    const month = ('0' + (d.getMonth() + 1)).slice(-2); 
+    const day = ('0' + d.getDate()).slice(-2); 
+    const year = d.getFullYear();
+    
+    return `${month}/${day}/${year}`;
   }
   
   submitPatient() {
