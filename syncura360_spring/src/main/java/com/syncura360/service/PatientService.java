8804dc209bb3ca961f6e88f4db73276d9474cf20
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2025-04-02 11:05:05 -0400

    SYN-113: Added update patient info API.

--- syncura360_spring/src/main/java/com/syncura360/service/PatientService.java
+++ syncura360_spring/src/main/java/com/syncura360/service/PatientService.java
@@ -1,6 +1,7 @@
 package com.syncura360.service;
 
 import com.syncura360.dto.Patient.PatientFormDTO;
+import com.syncura360.dto.Patient.PatientUpdateDTO;
 import com.syncura360.model.PatientInfo;
 import com.syncura360.model.enums.BloodType;
 import com.syncura360.model.enums.Gender;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Service
 public class PatientService {
@@ -83,5 +85,75 @@ public class PatientService {
 
         // Save new patient to database
         patientInfoRepository.save(newPatientInfo);
+    }
+
+    public void updatePatient(PatientUpdateDTO patientUpdateDTO) {
+        // Convert fields
+        LocalDate dateOfBirth = null;
+        if (patientUpdateDTO.getDateOfBirth() != null && !patientUpdateDTO.getDateOfBirth().trim().isEmpty()) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+            dateOfBirth = LocalDate.parse(patientUpdateDTO.getDateOfBirth().trim(), formatter);
+        }
+
+        Gender gender = null;
+        if (patientUpdateDTO.getGender() != null && !patientUpdateDTO.getGender().trim().isEmpty()) {
+            gender = Gender.valueOf(patientUpdateDTO.getGender().trim());
+        }
+
+        BloodType bloodType = null;
+        if (patientUpdateDTO.getBloodType() != null && !patientUpdateDTO.getBloodType().trim().isEmpty()) {
+            bloodType = BloodType.fromValue(patientUpdateDTO.getBloodType().trim());
+        }
+
+        // Find the patient if they already exists
+        Optional<PatientInfo> optionalPatientInfo = patientInfoRepository.findById(patientUpdateDTO.getId());
+        if (optionalPatientInfo.isEmpty()) {
+            throw new EntityExistsException("Given patient does not exist.");
+        }
+
+        // Check if fields are within constraints
+        if (patientUpdateDTO.getHeight() != null && patientUpdateDTO.getHeight() < 0) {
+            throw new IllegalArgumentException("Height must be a positive integer.");
+        }
+
+        if (patientUpdateDTO.getWeight() != null && patientUpdateDTO.getWeight() < 0) {
+            throw new IllegalArgumentException("Weight must be a positive integer.");
+        }
+
+        // Handle missing fields
+        String addressLine2 = patientUpdateDTO.getAddressLine2();
+        if (addressLine2 == null || addressLine2.trim().isEmpty()) addressLine2 = null;
+        else addressLine2 = addressLine2.trim();
+
+        String emergencyContactName = patientUpdateDTO.getEmergencyContactName();
+        if (emergencyContactName == null || emergencyContactName.trim().isEmpty()) emergencyContactName = null;
+        else emergencyContactName = emergencyContactName.trim();
+
+        String emergencyContactPhone = patientUpdateDTO.getEmergencyContactPhone();
+        if (emergencyContactPhone == null || emergencyContactPhone.trim().isEmpty()) emergencyContactPhone = null;
+        else emergencyContactPhone = emergencyContactPhone.trim();
+
+        // Update patient info
+        PatientInfo patientInfo = optionalPatientInfo.get();
+        patientInfo.setFirstName(patientUpdateDTO.getFirstName().trim());
+        patientInfo.setLastName(patientUpdateDTO.getLastName().trim());
+        patientInfo.setDateOfBirth(dateOfBirth);
+        patientInfo.setGender(gender);
+        patientInfo.setBloodType(bloodType);
+        patientInfo.setHeight(patientUpdateDTO.getHeight());
+        patientInfo.setWeight(patientUpdateDTO.getWeight());
+        patientInfo.setPhone(patientUpdateDTO.getPhone().trim());
+        patientInfo.setAddressLine1(patientUpdateDTO.getAddressLine1().trim());
+        patientInfo.setAddressLine2(addressLine2);
+        patientInfo.setCity(patientUpdateDTO.getCity().trim());
+        patientInfo.setState(patientUpdateDTO.getState().trim());
+        patientInfo.setPostal(patientUpdateDTO.getPostal().trim());
+        patientInfo.setCountry(patientUpdateDTO.getCountry().trim());
+        patientInfo.setEmergencyContactName(emergencyContactName);
+        patientInfo.setEmergencyContactPhone(emergencyContactPhone);
+
+        // Save the updates to the database
+        patientInfoRepository.save(patientInfo);
     }
 }
