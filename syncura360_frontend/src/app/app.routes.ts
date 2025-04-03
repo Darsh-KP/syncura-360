5bfe07e446b2d118b5775d3db9625d524892b6e7
Mubbashir Mahmood <mm3229@scarletmail.rutgers.edu>
2025-04-03 09:41:42 -0400

    SYN-110 - Functional Design

--- syncura360_frontend/src/app/app.routes.ts
+++ syncura360_frontend/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from './guards/auth.guard';
 import { SchedulingComponent } from './components/scheduling/scheduling.component';
 import {InventoryComponent} from './components/inventory/inventory.component';
 import { RoomManagementComponent } from './components/room-management/room-management.component';
+import {ServiceMgmtComponent} from './components/service-mgmt/service-mgmt.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -14,6 +15,7 @@ export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'scheduling', component: SchedulingComponent, canActivate: [AuthGuard] },
   { path: 'inventory', component: InventoryComponent, canActivate: [AuthGuard] },
+  { path: 'service-mgmt', component: ServiceMgmtComponent, canActivate: [AuthGuard] },
   {path: 'room-management', component: RoomManagementComponent, canActivate: [AuthGuard] },
   {
     path: 'nurse',
--- syncura360_frontend/src/app/components/inventory/inventory.component.ts
+++ syncura360_frontend/src/app/components/inventory/inventory.component.ts
@@ -8,7 +8,6 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { ItemFormComponent } from '../item-form/item-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-inventory',
@@ -99,17 +98,6 @@ export class InventoryComponent implements OnInit {
     });
   }
 
-  onQuantityEdit(): void {
-    const editedRows = this.gridApi.getRenderedNodes();
-    this.editedQuantities = {};
-
-    editedRows.forEach((row: any) => {
-      const original = this.inventoryList.find(i => i.ndc === row.data.ndc);
-      if (original && row.data.quantity !== original.quantity) {
-        this.editedQuantities[row.data.ndc] = row.data.quantity;
-      }
-    });
-  }
 
   onDelete(item: inventory): void {
     if (!item || !item.ndc) return;
