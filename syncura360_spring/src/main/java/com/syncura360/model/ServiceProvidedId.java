058db4aeb6050af9d660446c9e888643fc64c12e
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2025-04-04 23:04:21 -0400

    SYN-123: Created JPA models for Visit management.

---
+++ syncura360_spring/src/main/java/com/syncura360/model/ServiceProvidedId.java
@@ -0,0 +1,56 @@
+package com.syncura360.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.io.Serial;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor(force = true)
+@Getter
+@Embeddable
+public class ServiceProvidedId implements java.io.Serializable {
+    @Serial
+    private static final long serialVersionUID = 2148691830857429533L;
+
+    @Column(name = "hospital_id", nullable = false)
+    private final Integer hospitalId;
+
+    @Column(name = "patient_id", nullable = false)
+    private final Integer patientId;
+
+    @Column(name = "visit_admission_date_time", nullable = false)
+    private final LocalDateTime visitAdmissionDateTime;
+
+    @ColumnDefault("current_timestamp()")
+    @Column(name = "provided_at", nullable = false)
+    private LocalDateTime providedAt;
+
+    public ServiceProvidedId(Integer hospitalId, Integer patientId, LocalDateTime visitAdmissionDateTime) {
+        this.hospitalId = hospitalId;
+        this.patientId = patientId;
+        this.visitAdmissionDateTime = visitAdmissionDateTime;
+    }
+
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ServiceProvidedId entity = (ServiceProvidedId) o;
+        return Objects.equals(this.hospitalId, entity.hospitalId) &&
+                Objects.equals(this.patientId, entity.patientId) &&
+                Objects.equals(this.visitAdmissionDateTime, entity.visitAdmissionDateTime) &&
+                Objects.equals(this.providedAt, entity.providedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hospitalId, patientId, visitAdmissionDateTime, providedAt);
+    }
+}
---
+++ syncura360_spring/src/main/java/com/syncura360/model/Visit.java
@@ -0,0 +1,41 @@
+package com.syncura360.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(force = true)
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "Visit", schema = "syncura360")
+public class Visit {
+    @EmbeddedId
+    private final VisitId id;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "hospital_id", referencedColumnName = "hospital_id", nullable = false, insertable = false, updatable = false)
+    private Hospital hospital;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "patient_id", referencedColumnName = "patient_id", nullable = false, insertable = false, updatable = false)
+    private PatientInfo patient;
+
+    @Column(name = "discharge_date_time")
+    private LocalDateTime dischargeDateTime;
+
+    @Column(name = "reason_for_visit", length = 65535)
+    private String reasonForVisit;
+
+    @Column(name = "visit_summary", length = 65535)
+    private String visitSummary;
+
+    public Visit(VisitId id, String reasonForVisit) {
+        this.id = id;
+        this.reasonForVisit = reasonForVisit == null ? null : reasonForVisit.trim();
+    }
+}
