c78ee89c5a9d2bf7bb0fedcb2d32a661cc7b0630
Ben L. <ben.leiby25@gmail.com>
2025-04-11 13:01:05 -0400

    Updated.

--- syncura360_spring/src/main/java/com/syncura360/repository/VisitNoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.syncura360.repository;
-
-import com.syncura360.model.VisitNote;
-import com.syncura360.model.VisitNoteId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VisitNoteRepository extends JpaRepository<VisitNote, VisitNoteId> {
-}
