976a0f94257c6e623be5f2d062ab947526296c88
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2025-04-12 13:49:23 -0400

    SYN-91: Updated start app

--- syncura360_spring/src/main/java/com/syncura360/Syncura360SpringApplication.java
+++ syncura360_spring/src/main/java/com/syncura360/Syncura360SpringApplication.java
@@ -1,14 +1,12 @@
 package com.syncura360;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The entry point for the Syncura360 Spring application.
  */
- @SpringBootApplication
-@RequiredArgsConstructor
+@SpringBootApplication
 public class Syncura360SpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Syncura360SpringApplication.class, args);
