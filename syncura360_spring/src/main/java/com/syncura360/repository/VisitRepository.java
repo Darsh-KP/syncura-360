6ccf23cdedc1483d737e8bd952fdb2cf886d88d3
benleiby <105562274+benleiby@users.noreply.github.com>
2025-04-17 21:44:54 -0400

    Added record api.

--- syncura360_spring/src/main/java/com/syncura360/repository/VisitRepository.java
+++ syncura360_spring/src/main/java/com/syncura360/repository/VisitRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,24 @@ public interface VisitRepository extends JpaRepository<Visit, VisitId> {
             "WHERE vs.id.hospitalId = :hospitalId " +
             "AND vs.dischargeDateTime is NULL")
     List<Visit> findCurrentVisitsByHospitalId(
+            @Param("hospitalId") int hospitalId
+    );
+
+    @Query("SELECT vs FROM Visit vs " +
+            "WHERE vs.id.patientId = :patientId " +
+            "AND vs.id.admissionDateTime = :admissionDateTime " +
+            "AND vs.id.hospitalId = :hospitalId " +
+            "AND vs.dischargeDateTime IS NOT NULL")
+    Optional<Visit> findRecordById(
+            @Param("patientId") int patientId,
+            @Param("hospitalId") int hospitalId,
+            @Param("admissionDateTime") LocalDateTime admissionDateTime
+    );
+
+    @Query("SELECT vs FROM Visit vs " +
+            "WHERE vs.id.hospitalId = :hospitalId " +
+            "AND vs.dischargeDateTime IS NOT NULL")
+    List<Visit> findRecordsByHospitalId(
             @Param("hospitalId") int hospitalId
     );
 
