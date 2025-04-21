6c65b39eae439997420c943cdee0ee0b61948d84
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2025-04-20 23:11:34 -0400

    Created new test templates for testing.

---
+++ syncura360_spring/src/test/java/com/syncura360/service/DrugServiceTest.java
@@ -0,0 +1,29 @@
+package com.syncura360.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrugServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createDrug() {
+    }
+
+    @Test
+    void updateDrug() {
+    }
+
+    @Test
+    void deleteDrug() {
+    }
+
+    @Test
+    void fetchDrugs() {
+    }
+}
---
+++ syncura360_spring/src/test/java/com/syncura360/service/EquipmentServiceTest.java
@@ -0,0 +1,21 @@
+package com.syncura360.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EquipmentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setEquipmentsForRoom() {
+    }
+
+    @Test
+    void deleteEquipmentsForRoom() {
+    }
+}
